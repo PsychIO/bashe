17e5e84f4307d8187827b28a80a69e260433df29
Tychio <code@tychio.net>
2022-10-28 16:17:33 +0800

    improve class structure of source

--- source/example.py
+++ source/example.py
@@ -1,12 +1,14 @@
 import database.client as Client
 
 class Source:
+  db: None
+
   def __init__(self) -> None:
+    self.db = Client.DB()
     pass
 
   def query(self):
-    db = Client.DB()
-    result = db.query("""
+    result = self.db.query("""
       SELECT id, phone, status, is_active FROM leapq_samples
     """)
     return result
