736cfed622f4cb17fd744df91b061a3cef0fe03d
Tychio <code@tychio.net>
2022-10-28 16:42:46 +0800

    setup scipy lib

--- processor/example.py
+++ processor/example.py
@@ -1,6 +1,10 @@
+from scipy.stats import norm, kurtosis
+
 class Processor:
   def __init__(self) -> None:
     pass
 
   def process(self, data):
-    return [item for item in data if item['is_active'] == 1]
+    data = norm.rvs(size=1000, random_state=3)
+    kurtosis(data)
+    return data
--- source/example.py
+++ source/example.py
@@ -9,6 +9,10 @@ class Source:
 
   def query(self):
     result = self.db.query("""
-      SELECT id, phone, status, is_active FROM leapq_samples
+      SELECT 
+        id, name, phone, 
+        lang1_speaking_self as l1s, lang1_speaking_self as l2s,
+        lang1_listening_self as l1l, lang1_listening_self as l2l
+      FROM analysis_groups
     """)
     return result
